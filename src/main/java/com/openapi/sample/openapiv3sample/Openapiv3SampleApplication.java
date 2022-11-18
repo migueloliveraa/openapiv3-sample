d7a3b8593449019c3d654d1d1f5133ce21f8b589
molivera <miguel.olivera.a@gmail.com>
2022-11-17 23:04:46 -0500

    initial commit

---
+++ src/main/java/com/openapi/sample/openapiv3sample/Openapiv3SampleApplication.java
@@ -0,0 +1,13 @@
+package com.openapi.sample.openapiv3sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openapiv3SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openapiv3SampleApplication.class, args);
+	}
+
+}
